e27d768e1f95790ed6703c458eae9a9bdb03effe
Sergio Fariñas <sfarinasf22@fpcoruna.afundacion.org>
2023-12-08 18:10:50 +0100

    Añadida acción de rodar y enemigos que atacan

--- godot_juego/NPC_s/NpcArea2D.gd
+++ godot_juego/NPC_s/NpcArea2D.gd
@@ -16,7 +16,7 @@ func _input(event):
 			get_tree().paused=true
 			var dialog=Dialogic.start('timeline-1')
 			dialog.pause_mode=Node.PAUSE_MODE_PROCESS
-			dialog.connect('timeline-end',self,'unpause')
+			dialog.connect('timeline_end',self,'unpause')
 			add_child(dialog)
 
 func unpause(timeline_name):
--- godot_juego/Player/Player.gd
+++ godot_juego/Player/Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
-const ACCELERATION = 1500
-var MAX_SPEED = 240
-const FRICTION = 1500
+export var ACCELERATION = 300
+export var MAX_SPEED = 80
+export var ROLL_SPEED = 100
+export var FRICTION = 300
 
 enum{
 	MOVE,
@@ -11,13 +12,19 @@ enum{
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
+var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
 
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 func _physics_process(delta):
 	match state:
@@ -25,7 +32,7 @@ func _physics_process(delta):
 			move_state(delta)
 			
 		ROLL:
-			pass
+			roll_state(delta)
 			
 		ATTACK:
 			attack_state(delta)
@@ -38,26 +45,46 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state=ATTACK
-	
+
+
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 
+func move():
+	velocity = move_and_slide(velocity)
+
+func roll_animation_finished():
+	state = MOVE
+
 func attack_animation_finished():
 	state = MOVE
-	
 
-
+func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
